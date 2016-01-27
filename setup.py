66dfc3da4f058df24c549f0e227308039504fe6d
gchrlsyslib <syslib@gchrl.org>
2016-01-27 08:49:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='goodreads',
-    description="Python wrapper for Goodreads API",
+    description="Python wrapper for Goodreads API, adapted to import owned book lists to a Django project as a record attached to 
+    a user",
     long_description=open("README.rst").read(),
-    author='Sefa Kilic',
+    author='Sefa Kilic, adapted to Django by Sam Link',
     author_email='sefakilic@gmail.com',
     url='https://github.com/sefakilic/goodreads/',
     version='0.2.4',
